57179fa925b7096bd26e7fbd4c5b8581ef70b2a8
Dan Levin <dan@danlevin.dev>
2022-12-06 21:27:43 +0100

    day1 add tests

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,30 +1,78 @@
+fn solve_part1(input: &str) -> usize {
+    input
+        .split("\n\n")
+        .map(|x| {
+            x.split("\n")
+                .filter(|x| !x.is_empty())
+                .map(|x| x.parse::<usize>().unwrap())
+                .sum::<usize>()
+        })
+        .max()
+        .unwrap()
+}
+
+fn solve_part2(input: &str) -> usize {
+    let mut result = input.split("\n\n")
+        .map(|x| {
+            x.split("\n")
+                .filter(|x| !x.is_empty())
+                .map(|x| x.parse::<usize>().unwrap())
+                .sum::<usize>()
+        })
+        .collect::<Vec<usize>>();
+
+    result.sort_by(|a, b| b.cmp(a));
+    result[..3].iter().sum::<usize>()
+}
+
 fn main() {
     let input = include_str!("./day1_input.txt");
 
-    let part1 = input
-        .split("\n\n")
-        .map(|x| {
-            x.split("\n")
-                .filter(|x| !x.is_empty())
-                .map(|x| x.parse::<i32>().unwrap())
-                .sum::<i32>()
-        })
-        .max()
-        .unwrap();
+    let part1 = solve_part1(input);
 
-    let mut part2 = input
-        .split("\n\n")
-        .map(|x| {
-            x.split("\n")
-                .filter(|x| !x.is_empty())
-                .map(|x| x.parse::<i32>().unwrap())
-                .sum::<i32>()
-        })
-        .collect::<Vec<i32>>();
-
-    part2.sort_by(|a, b| b.cmp(a));
-    let part2 = part2[..3].iter().sum::<i32>();
+    let part2 = solve_part2(input);
 
     println!("part1: {}", part1);
     println!("part2: {}", part2);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_test_input() {
+        let input = include_str!("./day1_test.txt");
+
+        let result = solve_part1(input);
+
+        assert_eq!(result, 24000);
+    }
+
+    #[test]
+    fn part1_puzzle_input() {
+        let input = include_str!("./day1_input.txt");
+
+        let result = solve_part1(input);
+
+        assert_eq!(result, 71300);
+    }
+
+    #[test]
+    fn part2_test_input() {
+        let input = include_str!("./day1_test.txt");
+
+        let result = solve_part2(input);
+
+        assert_eq!(result, 45000);
+    }
+
+    #[test]
+    fn part2_puzzle_input() {
+        let input = include_str!("./day1_input.txt");
+
+        let result = solve_part2(input);
+
+        assert_eq!(result, 209691);
+    }
 }
